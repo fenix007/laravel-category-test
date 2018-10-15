54bc4731a5013f9b2185f3afe2d6861199fa4664
fenix007 <o4kan@ya.ru>
2018-10-15 11:16:24 +0300

    init nested set Category model

---
+++ app/Category.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App;
+
+use Baum\Node;
+
+class Category extends Node
+{
+}
---
+++ database/migrations/2018_10_15_080428_create_categories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            // These columns are needed for Baum's Nested Set implementation to work.
+            // Column names may be changed, but they *must* all exist and be modified
+            // in the model.
+            // Take a look at the model scaffold comments for details.
+            // We add indexes on parent_id, lft, rgt columns by default.
+            $table->increments('id');
+            $table->integer('parent_id')->nullable()->index();
+            $table->integer('lft')->nullable()->index();
+            $table->integer('rgt')->nullable()->index();
+            $table->integer('depth')->nullable();
+
+            $table->string('name', 255);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+    }
+}
