351dee7e8737d023f82eb3ecdad7a58dc2fa50fb
fenix007 <o4kan@ya.ru>
2018-10-15 11:07:00 +0300

    Install baum/baum

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Custom services
+         */
+        \Baum\Providers\BaumServiceProvider::class
     ],
 
     /*
