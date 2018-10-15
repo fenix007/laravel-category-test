707afe2a88b657ba6a4781713b2d0a4ff983fb8f
fenix007 <o4kan@ya.ru>
2018-10-15 16:42:56 +0300

    add product model with migration

--- app/Category.php
+++ app/Category.php
@@ -6,4 +6,7 @@ use Baum\Node;
 
 class Category extends Node
 {
+    public function products() {
+        return $this->belongsToMany(Product::class);
+    }
 }
---
+++ database/migrations/2018_10_15_084402_create_product_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_product', function(Blueprint $t) {
+            $t->unsignedInteger('product_id');
+            $t->unsignedInteger('category_id');
+
+            $t->primary(['product_id', 'category_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_product');
+    }
+}
